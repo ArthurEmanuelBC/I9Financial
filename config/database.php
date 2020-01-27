6a4e7b6a6508dd9e5d73b95bdc4aaf7ebf206e5a
Arthur Brandão <arthur_aebc@hotmail.com>
2020-01-27 08:57:12 -0300

    Database server

--- config/database.php
+++ config/database.php
@@ -68,8 +68,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
