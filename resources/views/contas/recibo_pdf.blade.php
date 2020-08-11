18c11811b83e248da0be8441b4f013f1f8f60cc5
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2020-08-11 16:23:50 -0300

    layout do recibo

--- resources/views/contas/recibo_pdf.blade.php
+++ resources/views/contas/recibo_pdf.blade.php
@@ -1,8 +1,10 @@
 @extends('template_pdf')
 @section('content')
 <style type="text/css">
-  table tr td { font-size: 24px; }
-  p { font-size: 18px; }
+  table { margin-bottom: 100px }
+  table tr td { font-size: 18px; }
+  p { font-size: 24px; }
+  #footer { bottom: 200px; }
 </style>
 
 <table class="no-border">
@@ -20,37 +22,23 @@
   </tr>
 </table>
 
-<div style="margin-top: 100px; margin-bottom: 100px">
+<div>
   <strong>
     <h1 align="center" style="float: left; width: 80%;">RECIBO</h1>
-    <h2 align="right" style="float: right; width: 20%;">Nº {{$conta->num_doc}}</h2>
+    <h3 align="right" style="float: right; width: 20%;">Nº {{$conta->num_doc}}</h3>
   </strong>
 </div>
 
-<p>
-  Recebemos do(a) Sr(a). @if($conta->paciente_ou_fornecedor()->pagador())
-  {{$conta->paciente_ou_fornecedor()->pagador()->nome}}, CPF nº
-  {{$conta->paciente_ou_fornecedor()->pagador()->cpf}}, @else
-  {{$conta->paciente_ou_fornecedor()->nome}}, CPF nº {{$conta->paciente_ou_fornecedor()->cpf}}, @endif a quantia de
-  R$
-  {{number_format($conta->valor,2,',','.')}}, referente a(s) despesa(s) decorrente de atendimento do(a) paciente:
-  {{$conta->paciente_ou_fornecedor()->nome}}, CPF nº {{$conta->paciente_ou_fornecedor()->cpf}}, neste estabelecimento.
+<p style="font-align: justify; position: absolute; top: 30%">
+  Recebemos do(a) Sr(a). @if($conta->paciente_ou_fornecedor()->pagador()) {{$conta->paciente_ou_fornecedor()->pagador()->nome}}, CPF nº {{$conta->paciente_ou_fornecedor()->pagador()->cpf}}, @else {{$conta->paciente_ou_fornecedor()->nome}}, CPF nº {{$conta->paciente_ou_fornecedor()->cpf}}, @endif a quantia de R$ {{number_format($conta->valor,2,',','.')}}, referente a(s) despesa(s) decorrente de atendimento do(a) paciente: {{$conta->paciente_ou_fornecedor()->nome}}, CPF nº {{$conta->paciente_ou_fornecedor()->cpf}}, neste estabelecimento.
 </p>
 <div id="footer">
-  <p>Atendido pelo médico: {{$conta->medico()->nome}}, CRM/PB {{$conta->medico()->crm}}, pelo qual dou plena e total
-    quitação. Com conhecimento do(a) paciente.</p>
-  <div style="text-align: center">
-    <p style="margin-top: 30px; margin-bottom: 30px">João Pessoa, {{@date_format(date_create_from_format('Y-m-d', $conta->date), 'd/m/Y')}}</p>
-    <p>{!! Html::image('/storage/empresas/'.$conta->medico()->id.'/'.$conta->medico()->anexo, "Assinatura", ['class' => 'assinaturas']) !!}</p>
-  </div>
+  <p style="text-align: justify">Atendido pelo médico: {{$conta->medico()->nome}}, CRM/PB {{$conta->medico()->crm}}, pelo qual dou plena e total quitação. Com conhecimento do(a) paciente.</p>
+  <p style="margin-top: 30px; margin-bottom: 60px">João Pessoa, {{@date_format(date_create_from_format('Y-m-d', $conta->date), 'd/m/Y')}}</p>
+  <p style="text-align: center">{!! Html::image('/storage/empresas/'.$conta->medico()->id.'/'.$conta->medico()->anexo, "Assinatura", ['class' => 'assinaturas']) !!}</p>
 </div>
 @endsection
 
 <style>
-  .assinatura {
-    max-width: 100px;
-  }
-  p {
-    text-align: justify;
-  }
+  .assinatura { max-width: 100px; }
 </style>
